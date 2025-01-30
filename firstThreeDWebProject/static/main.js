d81a933a34aba8155ac02e0e60b285d9179bb39e
bluebasecasinoBot <developerme33@gmail.com>
2025-01-30 11:21:42 +0100

    Update main.js

--- firstThreeDWebProject/static/main.js
+++ firstThreeDWebProject/static/main.js
@@ -10,7 +10,11 @@ __SYD.container = () =>{
         },
         [
             __SYD.MainCanvas(),
-            __SYD.audioSwitch()
+            __SYD.audioSwitch(),
+            __SYD.twitter(),
+            __SYD.tg(),
+            __SYD.c_a(),
+            __SYD.maxMin_btn()
         ]
     )
 }
@@ -24,6 +28,49 @@ __SYD.MainCanvas = () =>{
         [],
         {
             type:'MainCanvas'
+        }
+    )
+}
+
+__SYD.twitter = () =>{
+    return __c(
+        "a",
+        {
+            style:`position:fixed;top:70px;right:15px;background-image:url(./assets/image/x.png);background-size:cover;height:50px;width:50px;cursor:pointer;`,
+            href:"#",
+            title:"Twitter Link"
+        }
+    )
+}
+
+__SYD.tg = () =>{
+    return __c(
+        "a",
+        {
+            style:`position:fixed;top:125px;right:15px;background-image:url(./assets/image/tg.png);background-size:cover;height:50px;width:50px;cursor:pointer;`,
+            href:"#",
+            title:"telegram channel"
+        }
+    )
+}
+
+__SYD.c_a = () =>{
+    return __c(
+        'div',
+        {
+            style:'height:50px;width:50px;background-color:#d83f2a;cursor:pointer;background-size:50%;background-image:url("./assets/image/copy.png");position:fixed;top:180px;right:15px;border-radius:50%;',
+            class:'btn_copy',
+            title:"copy contract adx."
+        },[
+            __c("span" , {style:"min-width:max-content;position:absolute;top:calc(100% + 5px); left:50%;transform:translateX(-50%);text-transform:capitalize;color:#fff;font-weight:900;font-family:tahoma, sans serif;"},["copy C.A"])
+        ],{
+            genericStyle:['bg_fit'],
+            events:{
+                onclick:() =>{
+                    navigator.clipboard.writeText('c.a adx')
+                    alert('contract address copied')
+                }
+            }
         }
     )
 }
@@ -61,6 +108,43 @@ __SYD.audioSwitch = () =>{
                         video[1].muted = true;
                     }
                     __u("audioSwitch" , {type:"a" , value:state})
+                }
+            }
+        }
+    )
+}
+
+__SYD.maxMin_btn = () =>{
+    return __c(
+        "div",
+        {
+            style:`position:fixed;top:260px;right:15px;background-image:url(./assets/image/${__p(['maxMin_btn','url'] , 'max')}.png);background-size:cover;height:50px;width:50px;cursor:pointer;`,
+        },
+        [
+
+        ],
+        {
+            genericStyle:["bg_fit"],
+            createState:{
+                stateName:"maxMin_btn",
+                state:{
+                    url:"play"
+                }
+            },
+            events:{
+                onclick:e =>{
+                    const state = __g("maxMin_btn");
+                    state.url = state.url === "min" ? "max" : "min";
+                    // const video = getVideo1();
+                    if(document.fullscreenElement)
+                    {
+                        document.exitFullscreen()
+                    }
+                    else
+                    {
+                        document.body.requestFullscreen()
+                    }
+                    __u("maxMin_btn" , {type:"a" , value:state})
                 }
             }
         }
